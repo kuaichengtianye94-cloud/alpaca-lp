c280de2d4e1c6969cee8f353472aeca4cf004070
kuaichengtianye94-cloud <kuaichengtianye94@gmail.com>
2026-05-16 19:24:19 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'alpaca-v2';
+const ASSETS = [
+  '/app.html',
+  '/fee-calculator.html',
+  '/tel-manual.html',
+  '/manifest.json',
+  '/manifest-calc.json',
+  '/manifest-manual.json',
+  '/icon.svg',
+  '/icon-calc.svg',
+  '/icon-manual.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
